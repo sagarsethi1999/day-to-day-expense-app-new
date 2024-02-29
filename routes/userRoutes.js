ee2682b222189370d3c7d9d204dd00adc2e18221
sagarsethi1999 <sagarsethiputulu@gmail.com>
2024-02-29 09:03:05 +0530

    backend for login created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -27,4 +27,30 @@ router.post('/signup', async (req, res) => {
     }
 });
 
+
+
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // Check if user exists
+        const user = await User.findOne({ where: { email: email } });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Check if password matches
+        if (user.password !== password) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        // Password matches, login successful
+        return res.status(200).json({ message: 'User login successful' });
+    } catch (error) {
+        console.error('Error:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
 module.exports = router;
