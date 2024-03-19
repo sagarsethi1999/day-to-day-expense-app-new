7664b56235c2d8392bc96c9c27e6f52ab75fbee6
sagarsethi1999 <sagarsethiputulu@gmail.com>
2024-03-19 10:19:45 +0530

    database using env

--- util/database.js
+++ util/database.js
@@ -1,9 +1,11 @@
-const Sequelize = require ('sequelize');
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
-
-const sequelize = new Sequelize('day-to-day-expense', 'root','sagarhero143',
-{dialect: 'mysql',
-host: 'localhost'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    dialect: 'mysql',
+    host: process.env.DB_HOST
 });
+
 module.exports = sequelize;
